5b2127b2dca4188fe8b0040b7a1c94ccc8796bc4
SrVeraCruz <veracruzdudu@gmail.com>
2025-01-30 01:12:28 +0100

    feat: initial commit

---
+++ src/main/java/com/veracruz/uevents/UeventsApplication.java
@@ -0,0 +1,13 @@
+package com.veracruz.uevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veracruz/uevents/UeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veracruz.uevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
