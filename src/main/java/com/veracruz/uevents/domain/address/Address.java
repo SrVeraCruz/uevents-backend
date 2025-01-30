34af36bd6f37101bd5aecce0bad9604bf6d1a1a0
SrVeraCruz <veracruzdudu@gmail.com>
2025-01-30 01:34:11 +0100

    feat: create entities

---
+++ src/main/java/com/veracruz/uevents/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.veracruz.uevents.domain.address;
+
+import com.veracruz.uevents.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String country;
+    private String city;
+    private String state;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
